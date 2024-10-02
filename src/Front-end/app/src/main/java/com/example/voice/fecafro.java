54687b9b0d94bb1a07643cf84ee7ffe2890f0a79
FLAVIO JOSE DOS SANTOS <santos.flavio@edu.fecap.br>
2024-10-02 15:31:51 -0300

    [ADD]Adicionando a página do coletivo FECAFRO com botões para links externos - Entrega 02

--- src/Front-end/app/src/main/java/com/example/voice/fecafro.java
+++ src/Front-end/app/src/main/java/com/example/voice/fecafro.java
@@ -1,35 +1,44 @@
 package com.example.voice;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.annotation.NonNull;
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
-import androidx.core.splashscreen.SplashScreen;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class fecafro extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        SplashScreen.installSplashScreen(this);
+        setContentView(R.layout.activity_fecafro);
 
-        Button fecafro = findViewById(R.id.btn_fecafro);
-        fecafro.setOnClickListener(new View.OnClickListener() {
+        Button whats = findViewById(R.id.btn_whats);
+        whats.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, fecafro.class);
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://chat.whatsapp.com/LLIWaCdVEbkFAacmu3pUUJ"));
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        Button insta = findViewById(R.id.btn_insta);
+        insta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/Fecafro"));
                 startActivity(intent);
                 finish();
             }
@@ -41,9 +50,9 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
+
     }
 
 
@@ -51,13 +60,13 @@ public class MainActivity extends AppCompatActivity {
         int itemId = item.getItemId();
 
         if (itemId == R.id.nav_home) {
-            startActivity(new Intent(MainActivity.this, MainActivity.class));
+            startActivity(new Intent(fecafro.this, MainActivity.class));
             return true;
         } else if (itemId == R.id.nav_warning) {
-            startActivity(new Intent(MainActivity.this, Complaint.class));
+            startActivity(new Intent(fecafro.this, Complaint.class));
             return true;
         } else if (itemId == R.id.nav_image) {
-            startActivity(new Intent(MainActivity.this, Notice.class));
+            startActivity(new Intent(fecafro.this, Notice.class));
             return true;
         } else {
             return false;
