130784858d5ece493335ca5151eed283081dec06
wellingt0nn <wellington.4guiar1@gmail.com>
2024-10-02 19:33:06 -0300

    Entrega 2: criação da pagina Ruth Cardoso!

--- src/Front-end/app/src/main/java/com/example/voice/ruth_cardoso.java
+++ src/Front-end/app/src/main/java/com/example/voice/ruth_cardoso.java
@@ -1,58 +1,57 @@
 package com.example.voice;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.annotation.NonNull;
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
-import androidx.core.splashscreen.SplashScreen;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class ruth_cardoso extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        SplashScreen.installSplashScreen(this);
-
-            Button fecafro = findViewById(R.id.btn_fecafro);
-        fecafro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, fecafro.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-
-        Button ruthCardoso = findViewById(R.id.btn_ruth);
-        ruthCardoso.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ruth_cardoso.class); // Ensure class name matches your activity
-                startActivity(intent);
-                finish();
-            }
-        });
+        setContentView(R.layout.activity_ruth_cardoso);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
+        Button whats = findViewById(R.id.btn_whats);
+        whats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://chat.whatsapp.com/LLIWaCdVEbkFAacmu3pUUJ"));
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        Button insta = findViewById(R.id.btn_insta);
+        insta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/coletivo.ruthc/"));
+                startActivity(intent);
+                finish();
+            }
+        });
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
+
     }
 
 
@@ -60,13 +59,13 @@ public class MainActivity extends AppCompatActivity {
         int itemId = item.getItemId();
 
         if (itemId == R.id.nav_home) {
-            startActivity(new Intent(MainActivity.this, MainActivity.class));
+            startActivity(new Intent(ruth_cardoso.this, MainActivity.class));
             return true;
         } else if (itemId == R.id.nav_warning) {
-            startActivity(new Intent(MainActivity.this, Complaint.class));
+            startActivity(new Intent(ruth_cardoso.this, Complaint.class));
             return true;
         } else if (itemId == R.id.nav_image) {
-            startActivity(new Intent(MainActivity.this, Notice.class));
+            startActivity(new Intent(ruth_cardoso.this, Notice.class));
             return true;
         } else {
             return false;
