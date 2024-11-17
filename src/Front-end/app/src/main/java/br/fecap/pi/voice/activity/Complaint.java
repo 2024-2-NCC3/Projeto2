d29628d3e679bbdcfb6dbf917cf92c344f26c3e0
FLAVIO JOSE DOS SANTOS <santos.flavio@edu.fecap.br>
2024-11-17 20:18:37 -0300

    [CHG] Nova tela da home e botões expansíveis para navegação

--- src/Front-end/app/src/main/java/br/fecap/pi/voice/activity/Complaint.java
+++ src/Front-end/app/src/main/java/br/fecap/pi/voice/activity/Complaint.java
@@ -20,6 +20,7 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -33,6 +34,8 @@ public class Complaint extends AppCompatActivity {
     private Spinner dropdown;
     private EditText reportText;
     private Button sendButton;
+    private Button mainButton, complaintButton, fecafroButton;
+    private boolean isFabOpen = false;
 
 
     SimpleDateFormat sdf = new SimpleDateFormat("HH");
@@ -44,8 +47,11 @@ public class Complaint extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complaint);
 
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide(); // Remove a ActionBar
+        }
 
-        dropdown = findViewById(R.id.specificationSpinner);
+            dropdown = findViewById(R.id.specificationSpinner);
         reportText = findViewById(R.id.reportEditText);
         sendButton = findViewById(R.id.sendButton);
 
@@ -62,11 +68,62 @@ public class Complaint extends AppCompatActivity {
             }
         });
 
+        // Configurar o FloatingActionButton e os botões adicionais
+        FloatingActionButton fab = findViewById(R.id.floating_button);
+        mainButton = findViewById(R.id.main_button);
+        complaintButton = findViewById(R.id.denuncia_button);
+        fecafroButton = findViewById(R.id.ruth_button);
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
+        // Ocultar os botões inicialmente
+        mainButton.setVisibility(View.GONE);
+        complaintButton.setVisibility(View.GONE);
+        fecafroButton.setVisibility(View.GONE);
 
+        // Configurar o clique no FAB para mostrar/ocultar os botões
+        fab.setOnClickListener(view -> {
+            if (isFabOpen) {
+                closeFabMenu();
+            } else {
+                openFabMenu();
+            }
+        });
+
+        // Configurar os cliques nos botões para navegar para as atividades apropriadas
+        mainButton.setOnClickListener(view -> {
+            startActivity(new Intent(Complaint.this, MainActivity.class));
+        });
+
+        complaintButton.setOnClickListener(view -> {
+            startActivity(new Intent(Complaint.this, Complaint.class));
+        });
+
+        fecafroButton.setOnClickListener(view -> {
+            startActivity(new Intent(Complaint.this, ruth_cardoso.class));
+        });
     }
+
+    // Método para abrir o menu de botões com espaçamento ajustado
+    private void openFabMenu() {
+        mainButton.setVisibility(View.VISIBLE);
+        complaintButton.setVisibility(View.VISIBLE);
+        fecafroButton.setVisibility(View.VISIBLE);
+
+        // Ajuste o espaçamento vertical dos botões
+        mainButton.setTranslationY(-350f); // Distância maior
+        complaintButton.setTranslationY(-250f); // Distância intermediária
+        fecafroButton.setTranslationY(-150f); // Distância menor
+
+        isFabOpen = true;
+    }
+
+    // Método para fechar o menu de botões
+    private void closeFabMenu() {
+        mainButton.setVisibility(View.GONE);
+        complaintButton.setVisibility(View.GONE);
+        fecafroButton.setVisibility(View.GONE);
+        isFabOpen = false;
+    }
+
 
     private String cesarCipher(String text, int OFFSET){
         StringBuilder encryptedText = new StringBuilder();
@@ -107,25 +164,6 @@ public class Complaint extends AppCompatActivity {
 
 
         return encryptedText.toString();
-    }
-
-
-
-
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-        // this shit is very stupid but it works well
-
-        int itemId = item.getItemId();
-        if (itemId == R.id.nav_home) {
-            startActivity(new Intent(Complaint.this, MainActivity.class));
-            return true;
-        } else if (itemId == R.id.nav_warning) {
-            startActivity(new Intent(Complaint.this, Complaint.class));
-            return true;
-        } else {
-            return false;
-        }
     }
 
 
--- src/Front-end/app/src/main/java/br/fecap/pi/voice/activity/fecafro.java
+++ src/Front-end/app/src/main/java/br/fecap/pi/voice/activity/fecafro.java
@@ -7,14 +7,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import br.fecap.pi.voice.R;
 import br.fecap.pi.voice.adapter.Adapter;
@@ -27,12 +27,24 @@ public class fecafro extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private List<Noticia> listaNoticia = new ArrayList<>();
+    private Button mainButton, complaintButton, fecafroButton;
+    private boolean isFabOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_fecafro);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide(); // Remove a ActionBar
+        }
+
+        // Ajuste de insets para as barras do sistema
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         // Inicializar o RecyclerView
         recyclerView = findViewById(R.id.newsRecyclerView);
@@ -48,19 +60,6 @@ public class fecafro extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setHasFixedSize(true);
         recyclerView.setAdapter(adapter);
-    }
-
-    // Método para criar exemplos de notícias
-    private void criarNoticias() {
-        Noticia noticia;
-
-        noticia = new Noticia("https://www.save-free.com/cdn/https://scontent-sin2-1.cdninstagram.com/v/t51.29350-15/466803552_1751955152229960_1634597539392743335_n.jpg?stp=dst-jpg_e35_tt7&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyLmYyOTM1MC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=scontent-sin2-1.cdninstagram.com&_nc_cat=100&_nc_ohc=DmHum3BC2Q8Q7kNvgGYtPiY&_nc_gid=10da4dd1f0294ef780a3a0eec2bc847e&edm=ANTKIIoBAAAA&ccb=7-5&oh=00_AYBtyYf2EQXTHcnmh3y_-gUbjWMCHCNSgRlSww5tyEO8JQ&oe=673C5E13&_nc_sid=d885a2", "AFRO DAY 2024", "13/11/2024", "https://gazetadasemana.com.br/noticia/200609/afroday-2024-cultura-afro-brasileira-em-destaque-no-teatro-fecap#google_vignette");
-        listaNoticia.add(noticia);
-
-        noticia = new Noticia("https://www.save-free.com/cdn/https://scontent-fra3-1.cdninstagram.com/v/t51.29350-15/387271119_2629011913959927_1666745883246206419_n.jpg?stp=dst-jpg_e15_fr_s1080x1080_tt7&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyLmYyOTM1MC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=scontent-fra3-1.cdninstagram.com&_nc_cat=103&_nc_ohc=lqCZZKyPvNIQ7kNvgEy-6kY&_nc_gid=85e011578aee415db40faf85483110e5&edm=ANTKIIoBAAAA&ccb=7-5&oh=00_AYDwxXrH-wiZNoJCDG7_pz2du7TWlgkniajCjWbEQIo25g&oe=673C4C82&_nc_sid=d885a2", "Missão / Visão / Valores", "02/01/2024", "https://www.instagram.com/p/CyPLjCythq2/?igsh=MWg2dDFrcXhubGc5Mw==");
-        listaNoticia.add(noticia);
-
-        // Adicione mais notícias conforme necessário
 
         // Botão para WhatsApp
         Button whats = findViewById(R.id.btn_whats);
@@ -84,28 +83,74 @@ public class fecafro extends AppCompatActivity {
             }
         });
 
-        // Configurações para o Edge-to-Edge
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        // Configurar o FloatingActionButton e os botões adicionais
+        FloatingActionButton fab = findViewById(R.id.floating_button);
+        mainButton = findViewById(R.id.main_button);
+        complaintButton = findViewById(R.id.denuncia_button);
+        fecafroButton = findViewById(R.id.ruth_button);
+
+        // Ocultar os botões inicialmente
+        mainButton.setVisibility(View.GONE);
+        complaintButton.setVisibility(View.GONE);
+        fecafroButton.setVisibility(View.GONE);
+
+        // Configurar o clique no FAB para mostrar/ocultar os botões
+        fab.setOnClickListener(view -> {
+            if (isFabOpen) {
+                closeFabMenu();
+            } else {
+                openFabMenu();
+            }
         });
 
-//        // Navegação inferior
-//        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
-//        bottomNavigationView.setOnNavigationItemSelectedListener(this::onNavigationItemSelected);
+        // Configurar os cliques nos botões para navegar para as atividades apropriadas
+        mainButton.setOnClickListener(view -> {
+            startActivity(new Intent(fecafro.this, MainActivity.class));
+        });
+
+        complaintButton.setOnClickListener(view -> {
+            startActivity(new Intent(fecafro.this, Complaint.class));
+        });
+
+        fecafroButton.setOnClickListener(view -> {
+            startActivity(new Intent(fecafro.this, ruth_cardoso.class));
+        });
     }
 
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        int itemId = item.getItemId();
-        if (itemId == R.id.nav_home) {
-            startActivity(new Intent(fecafro.this, MainActivity.class));
-            return true;
-        } else if (itemId == R.id.nav_warning) {
-            startActivity(new Intent(fecafro.this, Complaint.class));
-            return true;
-        } else {
-            return false;
-        }
+    // Método para abrir o menu de botões com espaçamento ajustado
+    private void openFabMenu() {
+        mainButton.setVisibility(View.VISIBLE);
+        complaintButton.setVisibility(View.VISIBLE);
+        fecafroButton.setVisibility(View.VISIBLE);
+
+        // Ajuste o espaçamento vertical dos botões
+        mainButton.setTranslationY(-350f); // Distância maior
+        complaintButton.setTranslationY(-250f); // Distância intermediária
+        fecafroButton.setTranslationY(-150f); // Distância menor
+
+        isFabOpen = true;
+    }
+
+    // Método para fechar o menu de botões
+    private void closeFabMenu() {
+        mainButton.setVisibility(View.GONE);
+        complaintButton.setVisibility(View.GONE);
+        fecafroButton.setVisibility(View.GONE);
+        isFabOpen = false;
+    }
+
+    // Método para criar exemplos de notícias
+    private void criarNoticias() {
+        Noticia noticia;
+
+        noticia = new Noticia("https://www.save-free.com/cdn/https://scontent-sin2-1.cdninstagram.com/v/t51.29350-15/466803552_1751955152229960_1634597539392743335_n.jpg?stp=dst-jpg_e35_tt7&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyLmYyOTM1MC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=scontent-sin2-1.cdninstagram.com&_nc_cat=100&_nc_ohc=DmHum3BC2Q8Q7kNvgGYtPiY&_nc_gid=10da4dd1f0294ef780a3a0eec2bc847e&edm=ANTKIIoBAAAA&ccb=7-5&oh=00_AYBtyYf2EQXTHcnmh3y_-gUbjWMCHCNSgRlSww5tyEO8JQ&oe=673C5E13&_nc_sid=d885a2", "AFRO DAY 2024", "13/11/2024", "https://gazetadasemana.com.br/noticia/200609/afroday-2024-cultura-afro-brasileira-em-destaque-no-teatro-fecap#google_vignette");
+        listaNoticia.add(noticia);
+
+        noticia = new Noticia("https://www.save-free.com/cdn/https://scontent-fra3-1.cdninstagram.com/v/t51.29350-15/387271119_2629011913959927_1666745883246206419_n.jpg?stp=dst-jpg_e15_fr_s1080x1080_tt7&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xMDgweDEwODAuc2RyLmYyOTM1MC5kZWZhdWx0X2ltYWdlIn0&_nc_ht=scontent-fra3-1.cdninstagram.com&_nc_cat=103&_nc_ohc=lqCZZKyPvNIQ7kNvgEy-6kY&_nc_gid=85e011578aee415db40faf85483110e5&edm=ANTKIIoBAAAA&ccb=7-5&oh=00_AYDwxXrH-wiZNoJCDG7_pz2du7TWlgkniajCjWbEQIo25g&oe=673C4C82&_nc_sid=d885a2", "Missão / Visão / Valores", "02/01/2024", "https://www.instagram.com/p/CyPLjCythq2/?igsh=MWg2dDFrcXhubGc5Mw==");
+        listaNoticia.add(noticia);
+
+        // Adicione mais notícias conforme necessário
+
     }
 }
+
